2b7937433fc5a156bca7bced5379470698478055
Ha0Tang <haotang@pku.edu.cn>
2020-08-10 00:28:20 +0200

    delete DS_Store

---
+++ market_1501/train_market.sh
@@ -0,0 +1,3 @@
+export CUDA_VISIBLE_DEVICES=0;
+python train.py --dataroot ./SelectionGAN/person_transfer/datasets/market_data/ --name market_exp --model BiGraphGAN --lambda_GAN 5 --lambda_A 10  --lambda_B 10 --dataset_mode keypoint --no_lsgan --n_layers 3 --norm batch --batchSize 32 --resize_or_crop no --gpu_ids 0 --BP_input_nc 18 --no_flip --which_model_netG Graph --niter 500 --niter_decay 200 --checkpoints_dir ./checkpoints --pairLst ./SelectionGAN/person_transfer/datasets/market_data/market-pairs-train.csv --L1_type l1_plus_perL1 --n_layers_D 3 --with_D_PP 1 --with_D_PB 1  --display_id 0 
+#--continue_train --which_epoch 640 --epoch_count 641
