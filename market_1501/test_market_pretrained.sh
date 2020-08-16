ec74fab0dfbe8d245e09d7cbb4790b8e72604f3d
Ha0Tang <haotang@pku.edu.cn>
2020-08-16 17:03:02 +0200

    updated pretrained scripts

---
+++ market_1501/test_market_pretrained.sh
@@ -0,0 +1,2 @@
+export CUDA_VISIBLE_DEVICES=0;
+python test.py --dataroot ./SelectionGAN/person_transfer/datasets/market_data/ --name market_pretrained --model BiGraphGAN --phase test --dataset_mode keypoint --norm batch --batchSize 1 --resize_or_crop no --gpu_ids 0 --BP_input_nc 18 --no_flip --which_model_netG Graph --checkpoints_dir ./BiGraphGAN/scripts/checkpoints --pairLst ./SelectionGAN/person_transfer/datasets/market_data/market-pairs-test.csv --which_epoch latest --results_dir ./results/ --display_id 0;
