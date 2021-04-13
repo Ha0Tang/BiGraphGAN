1c126a3840c27c3069369df41017e5845838d4c1
Ha0Tang <haotang@pku.edu.cn>
2021-04-13 09:43:40 +0200

    fix typos

--- facial/scripts/download_bigraphgan_result.sh
+++ facial/scripts/download_bigraphgan_result.sh
@@ -4,11 +4,11 @@ echo "Note: available results are facial"
 echo "Specified [$FILE]"
 
 URL=http://disi.unitn.it/~hao.tang/uploads/results/BiGraphGAN/${FILE}_results.tar.gz
-TAR_FILE=./results_by_author/${FILE}_results.tar.gz
-TARGET_DIR=./results_by_author/${FILE}_results/
+TAR_FILE=../results_by_author/${FILE}_results.tar.gz
+TARGET_DIR=../results_by_author/${FILE}_results/
 
 wget -N $URL -O $TAR_FILE
 
 mkdir -p $TARGET_DIR
-tar -zxvf $TAR_FILE -C ./results_by_author/
+tar -zxvf $TAR_FILE -C ../results_by_author/
 rm $TAR_FILE
